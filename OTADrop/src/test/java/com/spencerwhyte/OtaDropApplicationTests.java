646935faba1d3cd2794053921d4a9657d04d5c66
Spencer Whyte <spencerwhyte@gmail.com>
2016-01-29 20:33:01 -0500

    Refactored everything into manageable chunks

--- OTADrop/src/test/java/com/spencerwhyte/OtaDropApplicationTests.java
+++ OTADrop/src/test/java/com/spencerwhyte/OtaDropApplicationTests.java
@@ -3,6 +3,9 @@ package com.spencerwhyte;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.spencerwhyte.configuration.OtaDropApplication;
+
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
